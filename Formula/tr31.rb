2992f7d04ffdf81c8b8cd7513f8fc80ab99a827d
Leon Lynch <lynch.leon@gmail.com>
2022-12-03 20:02:00 +0100

    Create formula for tr31-0.4.4
    Created using `brew create --cmake --tap openemv/tap https://github.com/openemv/tr31/releases/download/0.4.4/tr31-0.4.4-src.tar.gz`

---
+++ Formula/tr31.rb
@@ -0,0 +1,26 @@
+class Tr31 < Formula
+  desc "ASC X9 TR-31 library and tools"
+  homepage "https://github.com/openemv/tr31"
+  url "https://github.com/openemv/tr31/releases/download/0.4.4/tr31-0.4.4-src.tar.gz"
+  sha256 "6a595e363aa8c891b4af709827efac95273a6d239302232de2e457d4a8ad9848"
+  license "LGPL-2.1-or-later"
+
+  depends_on "cmake" => :build
+  depends_on "mbedtls@2"
+  depends_on "bash-completion" => :recommended
+  depends_on "doxygen" => :optional
+
+  on_macos do
+    depends_on "argp-standalone" => :build
+  end
+
+  def install
+    system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+  end
+
+  test do
+    system "cmake", "--build", "build", "--target", "test"
+  end
+end
