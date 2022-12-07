c1f259a20da69e90a691d812add4200ded03f152
Leon Lynch <lynch.leon@gmail.com>
2022-12-07 18:52:56 +0100

    Update formula for tr31-0.4.4
    * Add git builds to allow `brew install --HEAD tr31`
    * Build shared libraries
    * Remove testing. The brew testing phase is post-install, while the
      CMake testing phase is based on the build tree, not the installed
      files

--- Formula/tr31.rb
+++ Formula/tr31.rb
@@ -4,6 +4,7 @@ class Tr31 < Formula
   url "https://github.com/openemv/tr31/releases/download/0.4.4/tr31-0.4.4-src.tar.gz"
   sha256 "6a595e363aa8c891b4af709827efac95273a6d239302232de2e457d4a8ad9848"
   license "LGPL-2.1-or-later"
+  head "https://github.com/openemv/tr31.git", branch: "master"
 
   depends_on "cmake" => :build
   depends_on "mbedtls@2"
@@ -15,12 +16,12 @@ class Tr31 < Formula
   end
 
   def install
-    system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    system "cmake", "-S", ".", "-B", "build", *std_cmake_args, "-DBUILD_SHARED_LIBS=ON"
     system "cmake", "--build", "build"
     system "cmake", "--install", "build"
   end
 
   test do
-    system "cmake", "--build", "build", "--target", "test"
+    system "false"
   end
 end
