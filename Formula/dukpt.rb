073b3b57d537757e184b5816e92b67cdc3c8391f
Leon Lynch <lynch.leon@gmail.com>
2023-04-18 21:44:01 +0200

    Update dukpt formula to use dukpt-ui branch for HEAD build

--- Formula/dukpt.rb
+++ Formula/dukpt.rb
@@ -4,7 +4,10 @@ class Dukpt < Formula
   url "https://github.com/openemv/dukpt/releases/download/0.2.5/dukpt-0.2.5-src.tar.gz"
   sha256 "2124dd09dba28236fdd2651c14eda3c6787e9b0489cd2eb8758eb4b24548281b"
   license "LGPL-2.1-or-later"
-  head "https://github.com/openemv/dukpt.git", branch: "master"
+  head do
+    url "https://github.com/openemv/dukpt.git", branch: "dukpt-ui"
+    depends_on "qt@5"
+  end
 
   depends_on "cmake" => :build
   depends_on "mbedtls@2"
@@ -17,13 +20,31 @@ class Dukpt < Formula
   end
 
   def install
-    system "cmake", "-S", ".", "-B", "build",
-      *std_cmake_args,
-      "-DBUILD_SHARED_LIBS=YES",
-      "-DCMAKE_INSTALL_RPATH=#{rpath}",
-      "-DCMAKE_REQUIRE_FIND_PACKAGE_tr31=YES"
+    if build.head?
+      system "cmake", "-S", ".", "-B", "build",
+        *std_cmake_args,
+        "-DBUILD_SHARED_LIBS=YES",
+        "-DBUILD_DUKPT_UI=YES",
+        "-DBUILD_MACOSX_BUNDLE=YES",
+        "-DCMAKE_INSTALL_RPATH=#{rpath}"
+    else
+      system "cmake", "-S", ".", "-B", "build",
+        *std_cmake_args,
+        "-DBUILD_SHARED_LIBS=YES",
+        "-DCMAKE_INSTALL_RPATH=#{rpath}",
+        "-DCMAKE_REQUIRE_FIND_PACKAGE_tr31=YES"
+    end
+
     system "cmake", "--build", "build"
     system "cmake", "--install", "build"
+
+    if build.head? && OS.mac?
+      # Create dukpt-ui symlink in bin directory
+      bin.install_symlink prefix/"Dukpt.app/Contents/MacOS/Dukpt" => "dukpt-ui"
+
+      # For Dukpt to appear in Launchpad, do this:
+      # ln -s $(brew --prefix dukpt)/Dukpt.app /Applications/
+    end
   end
 
   test do
