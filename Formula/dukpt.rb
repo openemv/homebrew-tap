3fb70e20c46a3630200b6affcd6387d0f3902ba8
Leon Lynch <lynch.leon@gmail.com>
2023-04-23 15:57:33 +0200

    Update formula for dukpt-1.0.0

--- Formula/dukpt.rb
+++ Formula/dukpt.rb
@@ -1,17 +1,15 @@
 class Dukpt < Formula
   desc "ANSI X9.24 DUKPT libraries and tools"
   homepage "https://github.com/openemv/dukpt"
-  url "https://github.com/openemv/dukpt/releases/download/0.2.5/dukpt-0.2.5-src.tar.gz"
-  sha256 "2124dd09dba28236fdd2651c14eda3c6787e9b0489cd2eb8758eb4b24548281b"
-  license "LGPL-2.1-or-later"
-  head do
-    url "https://github.com/openemv/dukpt.git", branch: "dukpt-ui"
-    depends_on "qt@5"
-  end
+  url "https://github.com/openemv/dukpt/releases/download/1.0.0/dukpt-1.0.0-src.tar.gz"
+  sha256 "748d5e0ebd002b8ce6e5dd1fd4279750e3054c005c5e2f4a6b96a04581072908"
+  license "GPL-3.0-or-later"
+  head "https://github.com/openemv/dukpt.git", branch: "master"
 
   depends_on "cmake" => :build
   depends_on "mbedtls@2"
   depends_on "openemv/tap/tr31"
+  depends_on "qt@5"
   depends_on "bash-completion" => :recommended
   depends_on "doxygen" => :optional
 
@@ -20,25 +18,17 @@ class Dukpt < Formula
   end
 
   def install
-    if build.head?
-      system "cmake", "-S", ".", "-B", "build",
-        *std_cmake_args,
-        "-DBUILD_SHARED_LIBS=YES",
-        "-DBUILD_DUKPT_UI=YES",
-        "-DBUILD_MACOSX_BUNDLE=YES",
-        "-DCMAKE_INSTALL_RPATH=#{rpath}"
-    else
-      system "cmake", "-S", ".", "-B", "build",
-        *std_cmake_args,
-        "-DBUILD_SHARED_LIBS=YES",
-        "-DCMAKE_INSTALL_RPATH=#{rpath}",
-        "-DCMAKE_REQUIRE_FIND_PACKAGE_tr31=YES"
-    end
+    system "cmake", "-S", ".", "-B", "build",
+      *std_cmake_args,
+      "-DBUILD_SHARED_LIBS=YES",
+      "-DBUILD_DUKPT_UI=YES",
+      "-DBUILD_MACOSX_BUNDLE=YES",
+      "-DCMAKE_INSTALL_RPATH=#{rpath}"
 
     system "cmake", "--build", "build"
     system "cmake", "--install", "build"
 
-    if build.head? && OS.mac?
+    if OS.mac?
       # Create dukpt-ui symlink in bin directory
       bin.install_symlink prefix/"Dukpt.app/Contents/MacOS/Dukpt" => "dukpt-ui"
 
