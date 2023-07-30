38a1eb8390c8d1221d69f9322e77f69abbd600d8
Leon Lynch <lynch.leon@gmail.com>
2023-07-30 19:58:20 +0200

    Update formula for tr31-0.5.0
    * Update project description
    * Depend on latest version of MbedTLS

--- Formula/tr31.rb
+++ Formula/tr31.rb
@@ -1,13 +1,13 @@
 class Tr31 < Formula
-  desc "ASC X9 TR-31 library and tools"
+  desc "Key block library and tools for ANSI X9.143, ASC X9 TR-31 and ISO 20038"
   homepage "https://github.com/openemv/tr31"
-  url "https://github.com/openemv/tr31/releases/download/0.4.6/tr31-0.4.6-src.tar.gz"
-  sha256 "91e121ce056a6930652d8e260a4fac4f5adf125af916324db45913586893ef58"
+  url "https://github.com/openemv/tr31/releases/download/0.5.0/tr31-0.5.0-src.tar.gz"
+  sha256 "4c03649f3a536910aaa70113106a0f13d10e0e645772734ab553d52e351b3f0e"
   license "LGPL-2.1-or-later"
   head "https://github.com/openemv/tr31.git", branch: "master"
 
   depends_on "cmake" => :build
-  depends_on "mbedtls@2"
+  depends_on "mbedtls"
   depends_on "bash-completion" => :recommended
   depends_on "doxygen" => :optional
 
