dd6163fa396c97065cbc796a33924f1fede89d8a
Leon Lynch <lynch.leon@gmail.com>
2023-08-28 19:39:16 +0200

    Update formula for dukpt-1.1.0
    * Update license details
    * Depend on latest version of MbedTLS

--- Formula/dukpt.rb
+++ Formula/dukpt.rb
@@ -1,13 +1,13 @@
 class Dukpt < Formula
   desc "ANSI X9.24 DUKPT libraries and tools"
   homepage "https://github.com/openemv/dukpt"
-  url "https://github.com/openemv/dukpt/releases/download/1.0.0/dukpt-1.0.0-src.tar.gz"
-  sha256 "748d5e0ebd002b8ce6e5dd1fd4279750e3054c005c5e2f4a6b96a04581072908"
-  license "GPL-3.0-or-later"
+  url "https://github.com/openemv/dukpt/releases/download/1.1.0/dukpt-1.1.0-src.tar.gz"
+  sha256 "6bcce2507b8a2eb337e6690b2ac6fba4d772b24811c064fb0451f8968c5f6533"
+  license all_of: ["LGPL-2.1-or-later", "GPL-3.0-or-later"]
   head "https://github.com/openemv/dukpt.git", branch: "master"
 
   depends_on "cmake" => :build
-  depends_on "mbedtls@2"
+  depends_on "mbedtls"
   depends_on "openemv/tap/tr31"
   depends_on "qt@5"
   depends_on "bash-completion" => :recommended
