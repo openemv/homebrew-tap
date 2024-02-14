a22536f1650a8b00bc380ab182c3e1c91c47846e
Leon Lynch <lynch.leon@gmail.com>
2024-02-14 19:39:47 +0100

    Revise formula for dukpt-1.2.0 to use Qt5 instead
    It seems that Homebrew has broken macdeployqt for Qt6 again

--- Formula/dukpt.rb
+++ Formula/dukpt.rb
@@ -4,12 +4,13 @@ class Dukpt < Formula
   url "https://github.com/openemv/dukpt/releases/download/1.2.0/dukpt-1.2.0-src.tar.gz"
   sha256 "d6d905977df6b28ad68bb7b08c4b2003322c1edb69fffcc6313687deec6fbcbb"
   license all_of: ["LGPL-2.1-or-later", "GPL-3.0-or-later"]
+  revision 1
   head "https://github.com/openemv/dukpt.git", branch: "master"
 
   depends_on "cmake" => :build
   depends_on "mbedtls"
   depends_on "openemv/tap/tr31"
-  depends_on "qt@6"
+  depends_on "qt@5"
   depends_on "bash-completion" => :recommended
   depends_on "doxygen" => :optional
 
@@ -22,7 +23,7 @@ class Dukpt < Formula
       *std_cmake_args,
       "-DBUILD_SHARED_LIBS=YES",
       "-DBUILD_DUKPT_UI=YES",
-      "-DCMAKE_DISABLE_FIND_PACKAGE_Qt5=YES",
+      "-DCMAKE_DISABLE_FIND_PACKAGE_Qt6=YES",
       "-DBUILD_MACOSX_BUNDLE=YES",
       "-DCMAKE_INSTALL_RPATH=#{rpath}"
 
