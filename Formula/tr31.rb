6e3343280643d93b14fd87f889ac0fe99d35e4d7
Leon Lynch <lynch.leon@gmail.com>
2025-10-31 20:03:46 +0100

    Update formula for tr31-0.6.5

--- Formula/tr31.rb
+++ Formula/tr31.rb
@@ -1,9 +1,9 @@
 class Tr31 < Formula
   desc "Key block library and tools for ANSI X9.143, ASC X9 TR-31 and ISO 20038"
   homepage "https://github.com/openemv/tr31"
-  url "https://github.com/openemv/tr31/releases/download/0.6.4/tr31-0.6.4-src.tar.gz"
-  sha256 "a93659597541b59f6cdc8980e650eb044dc2c0e5748ebe70d2a956bbc04c8a1f"
-  license "LGPL-2.1-or-later"
+  url "https://github.com/openemv/tr31/releases/download/0.6.5/tr31-0.6.5-src.tar.gz"
+  sha256 "a9382b1e41fabb24a0d1549dc486875d78d59214396d0f2022c129893342c542"
+  license all_of: ["LGPL-2.1-or-later", "GPL-3.0-or-later"]
   head "https://github.com/openemv/tr31.git", branch: "master"
 
   depends_on "cmake" => :build
